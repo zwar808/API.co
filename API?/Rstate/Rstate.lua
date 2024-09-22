e179fd1c7452968842c3ff4e92371bc8b6496fa0
gg4exploiters <iloveexploiting123@gmail.com>
2024-09-22 17:52:45 +0800

    Create Rstate.lua

---
+++ API?/Rstate/Rstate.lua
@@ -0,0 +1,23 @@
+local include = {}
+local RstateUndefined = {}
+function rstate(type)
+    if type == nil or type == "" then
+        _G.Rstate = nil
+        table.insert(RstateUndefined, type)
+        return
+    end
+
+    if typeof(type) == "table" or typeof(type) == "boolean" or typeof(type) == "number" or typeof(type) == "string" or typeof(type) == "Color3" or typeof(type) == "Instance" then
+        _G.Rstate = type
+        table.insert(include, type)
+        return
+    end
+
+  if type == "Rstate" then
+      _G.Rstate = "?"
+    print("#include Rstate isint defined as a class.")
+  end
+    _G.Rstate = type
+    table.insert(include, type)
+  
+end
